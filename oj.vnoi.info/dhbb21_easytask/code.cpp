8619c70f7ecb4bb569b3da978cc9e501b1215276
Vo Dang Tue <vdtue0611@gmail.com>
2024-07-26 13:35:04 +0700

    commit

--- oj.vnoi.info/dhbb21_easytask/code.cpp
+++ oj.vnoi.info/dhbb21_easytask/code.cpp
@@ -1,8 +1,51 @@
 #include<bits/stdc++.h>
-
+#define int long long
 using namespace std;
 
-int main() {
-    
+const int MAXN = 1000005;
+
+int n;
+int a[MAXN], pf[MAXN];
+int res = -10000000000000, mn = 1000000000000;
+bool prime[MAXN];
+
+void init();
+
+signed main() {
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+    }
+
+    init();
+
+    for (int i = 1; i <= n; i++) {
+        if (prime[i]) {
+            mn = min(mn, pf[i - 1]);
+            res = max(res, pf[i] - mn);
+        }
+    }
+
+    cout << res;
     return 0;
+}
+
+void init()  {
+    for (int i = 2; i <= 1000000; i++) {
+        prime[i] = true;
+    }
+
+    for (int i = 2; i <= 1000; i++) {
+        if (prime[i]) {
+            for (int j = i * i; j <= 1000000; j += i) {
+                prime[j] = false;
+            }
+        }
+    }
+
+    for (int i = 1; i <= n; i++) {
+        pf[i] = pf[i - 1] + a[i];
+    }
 }
